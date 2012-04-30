dfcc5ec8ec1e64030f20905bddadb888eeee9816
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-04-30 12:09:37 +0200

    List The BSD 3-Clause License correctly and consistently. Thanks to Christian Aistleitner <christian@quelltextlich.at> for bringing this issue to my attention.

--- Tests/InvokerTest.php
+++ Tests/InvokerTest.php
@@ -40,7 +40,7 @@ declare(ticks = 1);
  * @subpackage Invoker
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @copyright  2011-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://github.com/sebastianbergmann/php-invoker
  * @since      File available since Release 1.0.0
  */
@@ -62,7 +62,7 @@ require_once FIXTURE_PATH . 'TestCallable.php';
  * @subpackage Invoker
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @copyright  2011-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @version    Release: @package_version@
  * @link       http://github.com/sebastianbergmann/php-invoker
  * @since      Class available since Release 1.0.0
