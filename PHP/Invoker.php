bfa7db87ca0ff16cf31801d6e269614deff1d80e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2013-01-07 11:36:02 +0100

    Bump copyright year

--- PHP/Invoker.php
+++ PHP/Invoker.php
@@ -4,7 +4,7 @@ declare(ticks = 1);
 /**
  * PHP_Invoker
  *
- * Copyright (c) 2011-2012, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * Copyright (c) 2011-2013, Sebastian Bergmann <sb@sebastian-bergmann.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -39,7 +39,7 @@ declare(ticks = 1);
  * @package    PHP
  * @subpackage Invoker
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @copyright  2011-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright  2011-2013 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://github.com/sebastianbergmann/php-invoker
  * @since      File available since Release 1.0.0
@@ -51,7 +51,7 @@ declare(ticks = 1);
  * @package    PHP
  * @subpackage Invoker
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @copyright  2011-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright  2011-2013 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @version    Release: @package_version@
  * @link       http://github.com/sebastianbergmann/php-invoker
--- PHP/Invoker/Autoload.php.in
+++ PHP/Invoker/Autoload.php.in
@@ -2,7 +2,7 @@
 /**
  * PHP_Invoker
  *
- * Copyright (c) 2011-2012, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * Copyright (c) 2011-2013, Sebastian Bergmann <sb@sebastian-bergmann.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,7 +37,7 @@
  * @package    PHP
  * @subpackage Invoker
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @copyright  2011-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright  2011-2013 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://github.com/sebastianbergmann/php-timer
  * @since      File available since Release 1.0.0
