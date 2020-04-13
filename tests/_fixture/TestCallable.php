e083fd3cf0437d2cf84396b8ecfe198c9d89fc14
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-04-13 08:59:07 +0200

    Fix CS/WS issue

--- tests/_fixture/TestCallable.php
+++ tests/_fixture/TestCallable.php
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 class TestCallable
 {
     public function test(int $sleep): bool
